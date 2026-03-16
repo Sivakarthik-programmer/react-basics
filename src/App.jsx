91532452af37a9f13a0c66c1e97a88ef55c78f07
Sivakarthik-programmer <sivakarthik385@gmail.com>
2026-03-16 10:15:53 +0530

    restructured the folder to keep the track of progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
-import { useState } from 'react'
-import Counter from './components/Counter'
-import NameInput from './components/NameInput'
-import './App.css'
-import ScoreBoard from './components/ScoreBoard'
-import UserCard from './components/UserCard'
+import React from 'react'
 
 function App() {
-  // 1. Create a state called "name" with your name as default value
-  const [name, setName] = useState('Sivakarthik')
-  
-  // 2. Create a state called "count" starting at 0
-  const [count, setCount] = useState(0)
-
-  const[playerName, setPlayerName] = useState('')
-  //  Expected behaviour:
-  // User types "Karthik" in ScoreBoard input
-  // Clicks submit
-  // App.jsx h2 shows "Player: Karthik"
-  
   return (
-    <>
-
-      <h1>Hello, {name}!</h1>
-      <h2>Player: {playerName}</h2>
-      <NameInput name={name} onChange={(e) => setName(e.target.value)} />
-      <Counter
-        count={count}
-        onIncrement={() => setCount(count + 1)}
-        onDecrement={() => setCount(count > 0 ? count - 1 : 0)}
-        onReset={() => setCount(0)}
-      />
-      <ScoreBoard onSubmit={(playerName) => setPlayerName(playerName)} />
-
-
-
-      <UserCard />
-    </>
+    <div>App</div>
   )
 }
 
